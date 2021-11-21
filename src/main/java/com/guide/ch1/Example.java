0dc4bd0c5edadb1785fbc094e9e4643de97ae577
mstfky <Gundeome42*>
2021-11-21 22:53:31 +0300

    Example.java added

---
+++ src/main/java/com/guide/ch1/Example.java
@@ -0,0 +1,9 @@
+package main.java.com.guide.ch1;
+/*This is a simple Java Program */
+public class Example {
+    public static void main(String args[]) {
+
+        System.out.println("This is a simple Java program");
+    }
+}
+
