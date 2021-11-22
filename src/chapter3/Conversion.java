760c6b76db42d008ef9115ad6c3f3622a3be2ef3
mstfky <Gundeome42*>
2021-11-22 22:57:14 +0300

    chapter 3 added

---
+++ src/chapter3/Conversion.java
@@ -0,0 +1,23 @@
+package chapter3;
+
+public class Conversion {
+    public static void main(String[] args) {
+        byte b;
+        int i = 257;
+        double d = 323.242;
+
+        System.out.println("Conversion of int to byte ");
+        b = (byte) i;
+        System.out.println(" i and b " + i + " " + b);
+
+        System.out.println("Conversion of double to int");
+        i = (int) d;
+        System.out.println(" d and i " + d + " " + i);
+
+        System.out.println("Conversion of double to byte");
+        b = (byte) d;
+        System.out.println(" b and d " + b + " " + d);
+
+
+    }
+}
